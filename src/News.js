59af9a73dc985b5f17c00bff06c3c25a9ffd0d9c
kanisreza <hajipour.mrh@gmail.com>
2021-02-05 17:28:36 +0100

    add API and more button

--- src/News.js
+++ src/News.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import ShowMore from "./ShowMore"
 
 const News = (props) => {
     return (
         <div className="news-div">
             <p>{props.title}</p>
+            <button onClick={props.handleClick}>{props.showMore? "Show less":"Show more!"}</button>
+            <br ></br>
+            {props.showMore &&  <ShowMore />}
+           
         </div>
     )
 }
---
+++ src/ShowMore.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ShowMore = (props) => {
+    return (
+        <div className="show-more">
+            <div>Author</div>
+            <div>Date</div>
+            <div>Link</div>
+        </div>
+    )
+}
+
+export default ShowMore;
