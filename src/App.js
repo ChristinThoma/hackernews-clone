d40764aac3b753b6445c30014404e383145c6235
kanisreza <hajipour.mrh@gmail.com>
2021-02-08 17:35:54 +0100

    add spiner

--- src/App.js
+++ src/App.js
@@ -1,79 +1,76 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import News from "./News";
-
+import CircularProgress from '@material-ui/core/CircularProgress';
 // import mock from "./data/mock.json";
 
 function App() {
-    const [data, setData] = useState(null);
-    // const [input, setInput] = useState("");
-    const [userSearch, setUserSearch] = useState("javaScript");
-    const [showMoreButton, setShowMoreButton] = useState(false);
+  const [data, setData] = useState(null);
+  // const [input, setInput] = useState("");
+  const [userSearch, setUserSearch] = useState("javaScript");
+  const [showMoreButton, setShowMoreButton] = useState(false);
 
-    const clickShowMoreButton = () => {
-        setShowMoreButton(true)
-    }
+  const clickShowMoreButton = () => {
+    setShowMoreButton(true)
+  }
 
-    //*****OLD VERSION USING MOCK DATA START******
-    //const searchFilter = () => {
-    //   let searchOutputArr = data.hits.filter((hit) => hit.title.toLowerCase().includes(userSearch))
-    //   if (searchOutputArr.length < 1) {
-    //     return (
-    //           <div className="no-match-div">
-    //               <p>THERE IS NO MATCH!</p>
-    //           </div>
-    //           )
-    //   }
-    //   else {
-    //     console.log(searchOutputArr)
-    //     return searchOutputArr.map((hit) => <News title={hit.title} />)
-    //   }
-    // }
-    ////*****OLD VERSION USING MOCK DATA END******
 
-    const handleInput = (event) => {
-        setUserSearch(event.target.value);
-        //we want to filter our array of objects by using includes method then map our array//
-    }
 
-    useEffect(() => {
-        fetch(`http://hn.algolia.com/api/v1/search?query=${userSearch}&tags=story`).then(response => {
-            if (response.ok) {
-                return response.json();
-            }
-            throw new Error('Request failed!');
-        }, networkError => {
-            console.log(networkError.message);
-        })
-            .then((jsonResponse) => {
-                setData(jsonResponse);
-            })
-    }, [userSearch])
+  //*****OLD VERSION USING MOCK DATA START******
+  //const searchFilter = () => {
+  //   let searchOutputArr = data.hits.filter((hit) => hit.title.toLowerCase().includes(userSearch))
+  //   if (searchOutputArr.length < 1) {
+  //     return (
+  //           <div className="no-match-div">
+  //               <p>THERE IS NO MATCH!</p>
+  //           </div>
+  //           )
+  //   }
+  //   else {
+  //     console.log(searchOutputArr)
+  //     return searchOutputArr.map((hit) => <News title={hit.title} />)
+  //   }
+  // }
+  ////*****OLD VERSION USING MOCK DATA END******
 
-    useEffect(() => {
-        console.log(data)
-    }, [data])
+  const handleInput = (event) => {
+    setUserSearch(event.target.value);
+    //we want to filter our array of objects by using includes method then map our array//
+  }
 
-    // const [searchMatch, setSearchMatch]= useState("");
-    // useEffect(()=>{
+  useEffect(() => {
+    fetch(`http://hn.algolia.com/api/v1/search?query=${userSearch}&tags=story`).then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error('Request failed!');
+    }, networkError => { console.log(networkError.message); })
+      .then((jsonResponse) => {
+        setData(jsonResponse);
+      })
+  }, [userSearch])
 
-    // }
-    // )
+  useEffect(() => {
+    
+  }, [data])
 
-    return (
-        <div>
-            <div><Header onEvent={handleInput}/></div>
-            <div className="news-body">{data && data.hits.map((hit) => <News key={hit.objectID} title={hit.title}
-                                                                             handleClick={clickShowMoreButton}
-                                                                             showMore={showMoreButton}/>)}</div>
-            <Footer/>
-        </div>
-    );
-}
+  // const [searchMatch, setSearchMatch]= useState("");
+  // useEffect(()=>{
 
-export default App;
+  // }
+  // )
 
-/* <div>{mock.hits.map((hit) => <News title={hit.title}/>)}</div>
-<div>{data && data.hits.filter((hit) => hit.title.toLowerCase().includes(input)).map((hit) => <News title={hit.title}/>)}
-*/
+  return (
+    <div>
+      <div><Header onEvent={handleInput} /></div>
+      <div className="news-body">{!data ? <CircularProgress /> : data.hits.map((hit) => <News key={hit.objectID} title={hit.title} handleClick={clickShowMoreButton} showMore={showMoreButton} />)}</div>
+      <Footer />
+    </div>
+  )};
+  export default App;
+
+
+// /* /* <div>{mock.hits.map((hit) => <News title={hit.title}/>)}</div> */
+/* <div>{data && data.hits.filter((hit) => hit.title.toLowerCase().includes(input)).map((hit) => <News title={hit.title}/>)}
+ */
