e102984f7755ec865c5dd64095dfd816f19180ed
ScholzRoman <romanscholz@hotmail.de>
2021-02-08 17:51:10 +0100

    Formatting

--- src/News.js
+++ src/News.js
@@ -3,10 +3,10 @@ import ShowMore from "./ShowMore"
 
 const News = (props) => {
     return (
-        <div id={props.id} className="news-div">
+        <div key={props.id} className="news-div">
             <p>{props.title}</p>
             <button onClick={props.handleClick}>{props.showMore ? "Show less" : "Show more!"}</button>
-            <br></br>
+            <br/>
             {props.showMore && <ShowMore allProps={props.allProps}/>}
         </div>
     )
